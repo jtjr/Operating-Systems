5803ea4df6323ecbd98790a37bb3e52a77c16189
Russell <sell_nat@yahoo.com>
2015-03-05 15:09:02 -0800

    SOLVED, if statement braces were breaking project

--- lab3fix/lab3.c
+++ lab3fix/lab3.c
@@ -1,60 +1,53 @@
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <ctype.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <ctype.h> 
+#include <sys/wait.h> 
 
 int doFib(int n); 
 
-int main(){
-	int val,n,k;
+int main()
+{
+	int val,i,k;
 	printf("Enter the number of a Fibonacci Sequence:\n");
-	scanf("%d", &n);
-	k = n;
-	val = 0;
-	for(k = 0; k <= n; k++)
+	scanf("%d", &i);
+
+	for(k = 0; k <= i; k++)
 	{
 		val = doFib(k);
-		printf("\n %d ",val);
+		printf("%d\n",val);
 	}
     exit(0);
 }
+
 int doFib(int n) 
 {
 
-  pid_t pid1, pid2;
+  pid_t pid1;
+  pid_t pid2;
 
-  if(n == 1)
-  {
-	  return 1;
-  }
-  else if(n == 0)
-  {
-	  return 0;
-  }
-  /*
   if( n < 2 )
-  {
-	  return(n);
-  }
-  */
+	return(n);
+  
+  
   pid1 = fork();
 
   if (pid1 == 0) 
-  {
-     exit(doFib(n-1));
-  }
+	exit(doFib(n-1));
   
-  int status1,status2;  
+  
+  int status1;
+  int status2; 
+   
   pid2 = fork();
 
   if (pid2 == 0) 
-  {
 	exit(doFib(n-2));
-  }
+  
   
   (void)waitpid(pid1,&status1,0);
   (void)waitpid(pid2,&status2,0);
 
-  return(WEXITSTATUS(status1)+WEXITSTATUS(status2));
+  return (WEXITSTATUS(status1)+WEXITSTATUS(status2));
 }
