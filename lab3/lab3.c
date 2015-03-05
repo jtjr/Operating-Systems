098e77106d3ad2f6699a9264db40a35e736dd971
Russell <sell_nat@yahoo.com>
2015-03-04 20:49:09 -0800

    need to clean up garbage

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -5,78 +5,58 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-int doFib(int n, int doPrint) {
+int doFib(int n) {
 
   int pid1, pid2;
   int status;
   int val;
   val = 0;
 
-  if (n<=1) 
+  if (n<=1) // Base case for the recursion
   {
-/*
-	  if(n == 1)
-	  {
-		  printf("1,");
-	  }
-	  else if(n == 0)
-	  {
-		  printf("0,");
-	  }
-	  //printf("%d",val);
-*/
-	  val = n;    // Base case for the recursion
-	  return(val);
+	  val = n;    
   }
   else {
 
     pid1 = fork();
 
     if (pid1 == 0) {
-      exit(doFib(n-1, 1));
+      exit(doFib(n-1));
     }
 
     else {              
       waitpid(pid1,&status,0);
-      if (WIFEXITED(status))
-        val = WEXITSTATUS(status);
+      val = WEXITSTATUS(status);
 
       pid2 = fork();
 
       if (pid2 == 0) {
-        exit(doFib(n-2, 0));
+        exit(doFib(n-2));
       }
 
       else {
         waitpid(pid2,&status,0);
-        if (WIFEXITED(status))
-          val += WEXITSTATUS(status);
+      
+        val += WEXITSTATUS(status);
       }
     }
   }
-
-  if (!doPrint) {
     return(val);
-  }
-  else if(val <2)
-  {
-	  //printf("%d,",val);
-	  exit(val);
-  }
-  else
-    printf("%d,", val);
-    exit(val);
 }
 
 int main(){
-	int n;//,k;
+	int n,k,printVal;
 	printf("Enter the number of a Fibonacci Sequence:\n");
 	scanf("%d", &n);
-	
-	//for( k = n; k >= 0; k--)
-	//{
-		doFib(n,1);
-	//}
+	k = n;
+	while( k >= 0)
+	{
+
+			printVal = doFib(k);
+			
+			printf("\n%d.) %d",k,printVal);
+		k--;
+	}
     
     
     return 0;
