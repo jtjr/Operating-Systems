637070ad2c6805a3d0526958116b45196f0dd49a
Russell <sell_nat@yahoo.com>
2015-03-05 14:50:27 -0800

    Im getting the right #'s but still printing child processes

--- lab3fix/lab3.c
+++ lab3fix/lab3.c
@@ -3,61 +3,58 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
-#include <stdbool.h>
 
-int doFib(int n) {
-
-  int pid1, pid2;
-  int status;
-  int val;
-  val = 0;
-
-  if (n<=1) // Base case for the recursion
-  {
-	  val = n;    
-  }
-  else {
-
-    pid1 = fork();
-
-    if (pid1 == 0) {
-      exit(doFib(n-1));
-    }
-
-    else {              
-      waitpid(pid1,&status,0);
-      val = WEXITSTATUS(status);
-
-      pid2 = fork();
-
-      if (pid2 == 0) {
-        exit(doFib(n-2));
-      }
-
-      else {
-        waitpid(pid2,&status,0);
-      
-        val += WEXITSTATUS(status);
-      }
-    }
-  }
-    return(val);
-}
+int doFib(int n); 
 
 int main(){
-	int n,k;
+	int val,n,k;
 	printf("Enter the number of a Fibonacci Sequence:\n");
 	scanf("%d", &n);
 	k = n;
-	while( k >= 0)
+	val = 0;
+	for(k = 0; k <= n; k++)
 	{
-
-			
-			
-			printf("\n%d.) %d ",k,doFib(k));
-		k--;
+		val = doFib(k);
+		printf("\n %d ",val);
 	}
-    
-    
-    return 0;
+    exit(0);
+}
+int doFib(int n) 
+{
+
+  pid_t pid1, pid2;
+
+  if(n == 1)
+  {
+	  return 1;
+  }
+  else if(n == 0)
+  {
+	  return 0;
+  }
+  /*
+  if( n < 2 )
+  {
+	  return(n);
+  }
+  */
+  pid1 = fork();
+
+  if (pid1 == 0) 
+  {
+     exit(doFib(n-1));
+  }
+  
+  int status1,status2;  
+  pid2 = fork();
+
+  if (pid2 == 0) 
+  {
+	exit(doFib(n-2));
+  }
+  
+  (void)waitpid(pid1,&status1,0);
+  (void)waitpid(pid2,&status2,0);
+
+  return(WEXITSTATUS(status1)+WEXITSTATUS(status2));
 }
