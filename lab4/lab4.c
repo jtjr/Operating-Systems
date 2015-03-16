8ecd32ba4ad90696937cdda2ffb2dc56c1666485
Russell <sell_nat@yahoo.com>
2015-03-16 16:03:30 -0700

    Lab 4 working

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -9,6 +9,8 @@ int min = 0;
 int max = 0;
 
 void *avg1(void *ptr);
+void *maxsum(void *ptr);
+void *minsum(void *ptr);
 
 int main(int argc, char *argv[])
 {
@@ -21,10 +23,14 @@ int main(int argc, char *argv[])
 	}
 	pthread_t thread1, thread2, thread3;
 	pthread_create(&thread1, NULL, (void *)avg1, (void *) &randA);
-	//pthread_create(&thread2, NULL, (void *)avg1, (void *) &randA);
-	//pthread_create(&thread3, NULL, (void *)avg1, (void *) &randA);
+	pthread_create(&thread2, NULL, (void *)maxsum, (void *) &randA);
+	pthread_create(&thread3, NULL, (void *)minsum, (void *) &randA);
 	pthread_join(thread1,NULL);
+	pthread_join(thread2,NULL);
+	pthread_join(thread3,NULL);
 	printf("avg = %d\n",avg);
+	printf("max = %d\n",max);
+	printf("min = %d\n",min);
 	return 0;
 
 }
@@ -42,7 +48,19 @@ void *avg1(void *ptr){
 }
 void *maxsum(void *ptr){
 	int i;
-	int sum = 0;
-	for(i = 0;
-
+	int temp = 0;
+	for(i = 0; i < n; i++)
+		if( randA[i]> max )
+			max = randA[i];
+		
+	pthread_exit(0);
+}
+void *minsum(void *ptr){
+	int i;
+	int temp = 0;
+	for(i = 0; i < n; i++)
+		if( randA[i]< min )
+			min = randA[i];
+		
+	pthread_exit(0);
 }
